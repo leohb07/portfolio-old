c5a3101e6cab66f8fa60ca0ea8e317c0cedcce7f
leohb07 <leohb1501@gmail.com>
2022-08-21 13:03:52 -0300

    Ajustando imagens, fonts, textos e adicionando projetos feitos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,17 @@
 const buttonElement = document.querySelector('header .nav-list #btn-mobile');
+const navLinks = document.querySelector('.h-list li a');
+const logoScrollToTop = document.querySelector('header .nav-list h2');
 
 function toggleMenu() {
   const navList = document.querySelector('header .nav-list');
-  navList.classList.toggle('active');
+  navList.classList.toggle('active');  
 };
 
 buttonElement.addEventListener('click', toggleMenu);
+navLinks.addEventListener('click', toggleMenu);
+
+function scrollToTop() {
+  window.scroll(0, 0);
+}
+
+logoScrollToTop.addEventListener('click', scrollToTop);
